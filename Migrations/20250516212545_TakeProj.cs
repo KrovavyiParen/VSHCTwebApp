d15e91de21c656a118d403418fa14e01459d5ca5
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2025-05-17 02:31:26 +0500

    взять проект

---
+++ Migrations/20250516212545_TakeProj.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VSHCTwebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class TakeProj : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TakenByTeamId",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TakenByTeamName",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "TakenByTeamId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "TakenByTeamName",
+                table: "Project");
+        }
+    }
+}
